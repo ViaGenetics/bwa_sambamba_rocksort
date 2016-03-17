333ebf0e911f3e63c7044ff01b258b36172ff3c9
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-17 09:20:49 -0400

    added .bam because it was not in variable

--- src/bwa_sambamba_rocksort.py
+++ src/bwa_sambamba_rocksort.py
@@ -235,7 +235,7 @@ def main(reads_1, reference, reference_index, read_group_sample, loglevel,
 
     sorted_bam = bam_files[0]
     markdup_bam = "out/output_markdups_bams/{0}.markdups.bam".format(read_group_sample)
-    sambamba_markdup_cmd = "sambamba markdup {0} -p -t {1} {2} {3}".format(
+    sambamba_markdup_cmd = "sambamba markdup {0} -p -t {1} {2}.bam {3}".format(
         advanced_sambamba_markdups_options, cpus, sorted_bam, markdup_bam)
     sambamba_markdup = dx_exec.execute_command(sambamba_markdup_cmd, debug=True)
     dx_exec.check_execution_syscode(sambamba_markdup, "Mark Duplicates")
